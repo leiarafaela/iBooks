7a9dcfc1c2ab7c39c28456f5c2e028b76bd9b405
Gabriel-Yuzo <gandrade529@gmail.com>
2023-03-16 18:35:08 -0300

    feat: connection and insert test

--- backend-server/model/db_connection.py
+++ backend-server/model/db_connection.py
@@ -1,22 +1,26 @@
-from dotenv import load_dotenv
 import mysql.connector
+from contextlib import closing
 import os
 
-
-load_dotenv()
-
-DB_HOST = os.getenv('DB_HOST')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
 
 
 def conectar_mysql():
     conn = mysql.connector.connect(
-        user=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        name=DB_NAME
+        user="b6a4da3f3869f6",
+        password="dd33ced0",
+        host="us-cdbr-east-06.cleardb.net",
+        database="heroku_315c522990c1dd4"
     )
 
     return conn
+
+def db_criar_clientes(nome, email, telefone):
+    with closing(conectar_mysql()) as con, closing(con.cursor()) as cur:
+        cur.execute("INSERT INTO clientes (nome, email, telefone) VALUES (%s, %s, %s)", [nome, email, telefone])
+        id_aluno = cur.lastrowid
+        con.commit()
+        return id_aluno
+    
+
+
+db_criar_clientes("Gabriel", "gabriel@gmail.com", "11-99495999")
