84608b9d50b7ee0b357b70161e54c949cc6f6596
Gabriel-Yuzo <gandrade529@gmail.com>
2023-03-21 21:42:17 -0300

    feat add outhers routes

--- app/controller/rotas.py
+++ app/controller/rotas.py
@@ -1,5 +1,7 @@
 from flask import Flask, make_response, jsonify, request
 from model.Customer import Customer as customer
+from model.Produtos import Produtos as produtos
+from model.Parceiros import Parceiros as parceiro
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -39,3 +41,81 @@ def update(customer_id):
     return make_response(jsonify(
         message='Cliente atualizado com sucesso.',
         customer=customer.getById(customer_id)))
+
+
+    ############################################################################
+    ##                                                                        ##
+    ##                         ROTAS DE PRODUTOS                              ##
+    ##                                                                        ##
+    ############################################################################
+
+
+@app.route('/produtos', methods=['GET'])
+def get_all_products():
+    return make_response(jsonify(
+        produtos.getAll()))
+
+@app.route('/produtos/<int:product_id>', methods=['GET'])
+def get_products(product_id):
+    return jsonify(
+        produtos.getById(product_id))
+
+@app.route('/produtos', methods=['POST'])
+def create_products():
+    body = request.json
+    produtos.create(body)
+    return make_response(jsonify(
+        message='Produto cadastrado com sucesso.'))
+
+@app.route('/produtos/<int:product_id>', methods=['DELETE'])
+def delete_products(product_id):
+    produtos.delete(product_id)
+    return make_response(jsonify(
+        message='Produto excluido com sucesso.'))
+
+@app.route('/produtos/<int:product_id>', methods=['PUT'])
+def update_products(product_id):
+    body = request.json
+    produtos.update(product_id, body)
+    return make_response(jsonify(
+        message='Produto atualizado com sucesso.',
+        produtos=produtos.getById(product_id)))
+
+
+    ############################################################################
+    ##                                                                        ##
+    ##                         ROTAS DE PARCEIROS                             ##
+    ##                                                                        ##
+    ############################################################################
+
+
+@app.route('/parceiros', methods=['GET'])
+def get_all_partners():
+    return make_response(jsonify(
+        parceiro.getAll()))
+
+@app.route('/parceiros/<int:parceiro_id>', methods=['GET'])
+def get_partner(parceiro_id):
+    return jsonify(
+        parceiro.getById(parceiro_id))
+
+@app.route('/parceiros', methods=['POST'])
+def create_partner():
+    body = request.json
+    parceiro.create(body)
+    return make_response(jsonify(
+        message='Parceiro cadastrado com sucesso.'))
+
+@app.route('/parceiros/<int:parceiro_id>', methods=['DELETE'])
+def delete_partners(parceiro_id):
+    parceiro.delete(parceiro_id)
+    return make_response(jsonify(
+        message='Parceiro excluido com sucesso.'))
+
+@app.route('/parceiros/<int:parceiro_id>', methods=['PUT'])
+def update_partners(parceiro_id):
+    body = request.json
+    parceiro.update(parceiro_id, body)
+    return make_response(jsonify(
+        message='Parceiro atualizado com sucesso.',
+        parceiro=parceiro.getById(parceiro_id)))
